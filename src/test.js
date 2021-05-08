7c2435b3de1cefad3209ee38d9b410e23b05c03d
Yetching <51228182+Yetching@users.noreply.github.com>
2021-05-08 18:51:32 +0800

    babel

---
+++ src/test.js
@@ -0,0 +1,14 @@
+const testFunc = () => {
+  console.log('函数测试');
+};
+
+const objA = {
+  name: 'li',
+  age: '20',
+};
+
+const arrA = [1, 2, 3, 4];
+
+const arrB = [8, 9, ...arrA];
+
+const strA = objA?.name;
---
+++ src/testInCode.js
@@ -0,0 +1,48 @@
+const parser = require('@babel/parser');
+const traverse = require('@babel/traverse').default;
+const generator = require('@babel/generator').default;
+const types = require('@babel/types');
+
+const sourceFileName = 'testInCode.js';
+const sourceCode = `
+  console.log(1)
+  function log(): number {
+    console.debug('before')
+    console.error(2)
+    console.debug('after')
+  }
+  log()
+  class Foo {
+    bar(): void {
+      console.log(3)
+    }
+    render() {
+      return <div></div>
+    }
+  }  
+`;
+
+const ast = parser.parse(sourceCode, {
+  plugins: ['typescript', 'jsx'],
+});
+
+traverse(ast, {
+  CallExpression(path) {
+    const calleeStr = generator(path.node.callee).code;
+    if (['console.log', 'console.error'].includes(calleeStr)) {
+      const { line, column } = path.node.loc.start;
+      path.node.arguments.unshift(
+        types.stringLiteral(`${sourceFileName}(${line}, ${column}):`)
+      );
+    }
+  },
+});
+
+const { code, map } = generator(ast, {
+  sourceMaps: true,
+  sourceFileName,
+});
+
+console.log(code);
+
+console.log(map);
